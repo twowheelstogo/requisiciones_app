efdf7c6278855ae4a70111e17c863d634d0755c6
Iván Arango <73449144+IvanArango98@users.noreply.github.com>
2022-03-14 09:47:05 -0600

    autenticacion

--- requisicion_viaticos_app/lib/CrearUsuario/Metodos.dart
+++ requisicion_viaticos_app/lib/CrearUsuario/Metodos.dart
@@ -2,33 +2,29 @@ import 'package:http/http.dart' as http;
 import 'package:requisicion_viaticos_app/Config/constants.dart';
 import 'dart:convert';
 
- Future<List> CrearUsuario_(String NombreUsuario,String pass) async {
-    bool bandera = false;
-    String Mensaje = "";    
-    final Response = await getAuthUserInfo(NombreUsuario);
-    final Decoded = json.decode(Response.body);
-
-    if (Response.statusCode == 200) {
-      if (Decoded["records"].length > 0) {                
-        var ID_AIRTABLE = Decoded["records"][0]["id"];    
-        final Response2 = await crearUsuarioFinal(ID_AIRTABLE,pass);        
-        if(Response2.statusCode != 200)
-        {
-          bandera = true;
-          Mensaje = "Ha ocurrido un error, intente nuevamente.";
-        }else{
-            Mensaje = "Credenciales generadas exitosamente.";
-        }
-
-      } else {
-        bandera = true;
-        Mensaje = "Credenciales incorrectas.";
-      }
-    } else {
-      bandera = true;
-      Mensaje = "Ha ocurrido un error, intente nuevamente.";
+ Future<List> CrearUsuario_(String NombreUsuario,String pass) async {  
+    List Res = await validateUser(NombreUsuario);          
+   
+    if(Res[0].toString().length == 0)
+    {
+        final Response = await crearUsuarioFinal(Res[1].toString(),pass);
+          if (Response.statusCode == 200) {
+               final Decoded = json.decode(Response.body);  
+               if (Decoded["records"].length > 0) {  
+                  return [false,"Credenciales generadas exitosamente."];
+               } 
+               else{
+                 return [true,'Ha ocurrido un error, intente nuevamente.'];
+               }
+          }  
+          else{
+               return [true,'Ha ocurrido un error, intente nuevamente.'];
+          }
     }
-    return [bandera, Mensaje];
+    else{
+      return [true,Res[0].toString()];
+    }
+
   }
 
    Future<http.Response> crearUsuarioFinal(String ID,String pass) async
@@ -40,7 +36,8 @@ import 'dart:convert';
     };
     
     Map<String, dynamic> body = {
-      "CONTRASEÑA" : pass
+      "CONTRASEÑA" : pass,
+      "CAMBIO CONTRASEÑA" : "NO DISPONIBLE"
     };
 
      final bodyEncoded = json.encode({
@@ -59,11 +56,60 @@ import 'dart:convert';
     }
   }
 
+  Future<List> validateUser(String user) async {
+    String mensaje = "";
+    final Response = await getAuthUserInfo(user);
+    final Decoded = json.decode(Response.body);
+    var ID = "";
+
+    if (Response.statusCode == 200) {
+      if (Decoded["records"].length > 0) { 
+        final Response2 = await getDisponibiltyPass(user);       
+        if(Response2.statusCode == 200)
+        {
+          
+           final Decoded2 = json.decode(Response2.body);            
+           if (Decoded2["records"].length > 0) { 
+             ID = Decoded2["records"][0]["id"]; 
+             mensaje = "";
+           }
+           else{
+             mensaje = "Notifique a su encargado para poder cambiar su contraseña nuevamente.";
+           }
+        }
+        else{
+          mensaje = "Ha ocurrido un error, intente de nuevo.";
+        }
+      }
+      else{
+        mensaje = "No de DPI incorrecto.";
+      }
+    }
+    else{
+      mensaje = "Ha ocurrido un error, intente de nuevo.";
+    }
+
+    return [mensaje,ID];
+  }
+
    Future<http.Response> getAuthUserInfo(String NombreUsuario) async {
     String url = urlApi +
-        "COLABORADORES?filterByFormula=AND({No_DOCUMENTO}='" +
+        "COLABORADORES?filterByFormula=AND(({No_DOCUMENTO}=" +
+        NombreUsuario + "))";
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': "Bearer $Token"
+    };
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
+    return response;
+  }
+
+Future<http.Response> getDisponibiltyPass(String NombreUsuario) async {
+    String url = urlApi +
+        "COLABORADORES?filterByFormula=AND(({No_DOCUMENTO}=" +
         NombreUsuario +
-        "')";
+        "),({CAMBIO CONTRASEÑA} = 'DISPONIBLE'))";
     Map<String, String> headers = {
       'Content-type': 'application/json',
       'Accept': 'application/json',
